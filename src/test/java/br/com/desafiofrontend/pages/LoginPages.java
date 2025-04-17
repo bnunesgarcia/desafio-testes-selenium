b4d4af60f1f840ce86e645d392d596a83e02919b
bnunesgarcia <brung@br.ibm.com>
2025-04-17 13:21:32 -0300

    Atualizacao do page objects

--- src/test/java/br/com/desafiofrontend/pages/LoginPages.java
+++ src/test/java/br/com/desafiofrontend/pages/LoginPages.java
@@ -10,12 +10,22 @@ import com.github.javafaker.Faker;
 import java.time.Duration;
 import java.util.NoSuchElementException;
 import br.com.desafiofrontend.support.Utils;
-import constants.Variaveis;
 
 public class LoginPages extends Utils {
 
+    String botaoTelaNewUser = "//*[@id=\"newUser\"]";
+    String campoFirstName = "//*[@id=\"firstname\"]";
+    String campoLastName = "//*[@id=\"lastname\"]";
+    String campoUserName = "//*[@id=\"userName\"]";
+    String campoPassword = "//*[@id=\"password\"]";
+    String botaoRegistrar = "//*[@id=\"register\"]";
+    String botaoLogin = "//*[@id=\"login\"]";
+    String botaoLogout = "//*[@id=\"submit\"]";
+    String containerMensagem = "//*[@id=\"name\"]";
+    String containerNomeUser = "//*[@id=\"userName-value\"]";
+
     public void acessaTelaNovoUsuario(){
-        driver.findElement(By.xpath(Variaveis.botaoTelaNewUser)).click();
+        driver.findElement(By.xpath(botaoTelaNewUser)).click();
     }
 
     public void cadastraNovoUsuario() throws InterruptedException{
@@ -32,10 +42,10 @@ public class LoginPages extends Utils {
             recaptchaContainer.click();
         }
         driver.switchTo().defaultContent();
-        driver.findElement(By.xpath(Variaveis.campoFirstName)).sendKeys(firstName);
-        driver.findElement(By.xpath(Variaveis.campoLastName)).sendKeys(lastName);
-        driver.findElement(By.xpath(Variaveis.campoUserName)).sendKeys(userName);
-        driver.findElement(By.xpath(Variaveis.campoPassword)).sendKeys(password);
+        driver.findElement(By.xpath(campoFirstName)).sendKeys(firstName);
+        driver.findElement(By.xpath(campoLastName)).sendKeys(lastName);
+        driver.findElement(By.xpath(campoUserName)).sendKeys(userName);
+        driver.findElement(By.xpath(campoPassword)).sendKeys(password);
 
         try {
             WebElement adIframe = driver.findElement(By.id("google_ads_iframe_/21849154601,22343295815/Ad.Plus-Anchor_0"));
@@ -43,49 +53,49 @@ public class LoginPages extends Utils {
         } catch (NoSuchElementException e) {
 
         }
-        WebElement registerButton = driver.findElement(By.xpath(Variaveis.botaoRegistrar));
+        WebElement registerButton = driver.findElement(By.xpath(botaoRegistrar));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", registerButton);
         // Thread.sleep(10000);
     }
 
     public void realizarLogin(){
-        driver.findElement(By.xpath(Variaveis.campoUserName)).isDisplayed();
-        driver.findElement(By.xpath(Variaveis.campoUserName)).sendKeys("TesteBru");
-        driver.findElement(By.xpath(Variaveis.campoPassword)).sendKeys("Teste@1000");
-        driver.findElement(By.xpath(Variaveis.botaoLogin)).click();
-        WebElement nomeUsuarioLogado = driver.findElement(By.xpath(Variaveis.containerNomeUser));
+        driver.findElement(By.xpath(campoUserName)).isDisplayed();
+        driver.findElement(By.xpath(campoUserName)).sendKeys("TesteBru");
+        driver.findElement(By.xpath(campoPassword)).sendKeys("Teste@1000");
+        driver.findElement(By.xpath(botaoLogin)).click();
+        WebElement nomeUsuarioLogado = driver.findElement(By.xpath(containerNomeUser));
         String elemento = nomeUsuarioLogado.getText();
         Assert.assertEquals(elemento, "TesteBru");
     }
 
     public void realizaLogout(){
-        driver.findElement(By.xpath(Variaveis.botaoLogout)).isDisplayed();
-        driver.findElement(By.xpath(Variaveis.botaoLogout)).click();
+        driver.findElement(By.xpath(botaoLogout)).isDisplayed();
+        driver.findElement(By.xpath(botaoLogout)).click();
     }
 
     public void realizarLoginNegativo(String motivo_erro){
         if(motivo_erro.equals("senha invalida")){
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(Variaveis.campoUserName)));
-            driver.findElement(By.xpath(Variaveis.campoUserName)).sendKeys("TesteBru");
-            driver.findElement(By.xpath(Variaveis.campoPassword)).sendKeys("teste");
-            driver.findElement(By.xpath(Variaveis.botaoLogin)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(campoUserName)));
+            driver.findElement(By.xpath(campoUserName)).sendKeys("TesteBru");
+            driver.findElement(By.xpath(campoPassword)).sendKeys("teste");
+            driver.findElement(By.xpath(botaoLogin)).click();
         } else if(motivo_erro.equals("usuario invalido")){
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(Variaveis.campoUserName)));
-            driver.findElement(By.xpath(Variaveis.campoUserName)).sendKeys("userInv");
-            driver.findElement(By.xpath(Variaveis.campoPassword)).sendKeys("Teste@2000");
-            driver.findElement(By.xpath(Variaveis.botaoLogin)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(campoUserName)));
+            driver.findElement(By.xpath(campoUserName)).sendKeys("userInv");
+            driver.findElement(By.xpath(campoPassword)).sendKeys("Teste@2000");
+            driver.findElement(By.xpath(botaoLogin)).click();
         }
     }
 
     public void validaFalhaNoLogin(String motivo_erro, String mensagem){
         if(motivo_erro.equals("senha invalida")){
-            WebElement mensagemContainer = driver.findElement(By.xpath(Variaveis.containerMensagem));
+            WebElement mensagemContainer = driver.findElement(By.xpath(containerMensagem));
             String elemento = mensagemContainer.getText();
             Assert.assertEquals(elemento, mensagem);
         } else if(motivo_erro.equals("usuario invalido")){
-            WebElement mensagemContainer = driver.findElement(By.xpath(Variaveis.containerMensagem));
+            WebElement mensagemContainer = driver.findElement(By.xpath(containerMensagem));
             String elemento = mensagemContainer.getText();
             Assert.assertEquals(elemento, mensagem);
         }
--- src/test/java/br/com/desafiofrontend/pages/NavegacaoPages.java
+++ src/test/java/br/com/desafiofrontend/pages/NavegacaoPages.java
@@ -6,46 +6,95 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import br.com.desafiofrontend.support.Utils;
-import constants.Variaveis;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
 
 public class NavegacaoPages extends Utils {
+
+    List<String> getSubmenus(String menu) {
+        switch (menu.toLowerCase()) {
+            case "elements":
+                return Arrays.asList("Text Box", "Check Box", "Radio Button", "Web Tables", "Buttons", "Links", "Broken Links - Images", "Upload and Download", "Dynamic Properties");
+            case "forms":
+                return Arrays.asList("Practice Form");
+            case "alerts, frame & windows":
+                return Arrays.asList("Browser Windows", "Alerts", "Frames", "Nested Frames", "Modal Dialogs");
+            case "widgets":
+                return Arrays.asList("Accordian", "Auto Complete", "Date Picker", "Slider", "Progress Bar", "Tabs", "Tool Tips", "Menu", "Select Menu");
+            case "interactions":
+                return Arrays.asList("Sortable", "Selectable", "Resizable", "Droppable", "Dragabble");
+            case "book store application":
+                return Arrays.asList("Login", "Book Store", "Profile", "Book Store API");
+            default:
+                return Arrays.asList();
+        }
+    }
+
+    String[][] REGISTROS = {
+        {"Vega", "Cierra", "Vega", "39", "cierra@example.com", "10000", "Insurance"},
+        {"Cantrell", "Alden", "Cantrell", "45", "alden@example.com", "12000", "Compliance"},
+        {"Gentry", "Kierra", "Gentry", "29", "kierra@example.com", "2000", "Legal"}
+    };
+
+    String menuForms = "//div[2]/div/div/div[2]/div/div[2]";
+    String menuAlerts = "//div[2]/div/div/div[2]/div/div[3]";
+    String menuElements = "//div[2]/div/div/div[2]/div/div[1]";
+    String widgets = "//div[2]/div/div/div[2]/div/div[4]";
+    String menuInteractions = "//div[2]/div/div/div[2]/div/div[5]";
+    String menuBookStoreApplication = "//div[2]/div/div/div[2]/div/div[6]";
+    String subMenuPracticeForms = "//div[2]/div/div/div/div[1]/div/div/div[2]/div/ul";
+    String subMenuBrowserWindows = ".show > ul:nth-child(1) > li:nth-child(1) > span:nth-child(2)";
+    String subMenuWebElements = ".show > ul:nth-child(1) > li:nth-child(4) > span:nth-child(2)";
+    String subMenuSortable = ".show > ul:nth-child(1) > li:nth-child(1)";
+    String subMenuLogin = ".show > ul:nth-child(1) > li:nth-child(1)";
+    String campoPesquisa = "//*[@id=\"searchBox\"]";
+    String botaoAddNovoRegistro = "//*[@id=\"addNewRecordButton\"]";
+    String botaoEditaRegistro = "//*[@id=\"edit-record-1\"]";
+    String botaoDeletaRegistro = "//*[@id=\"delete-record-1\"]";
+    String campoNome = "firstName";
+    String campoSobrenome = "lastName";
+    String campoEmail = "userEmail";
+    String campoIdade = "age";
+    String campoSalario = "salary";
+    String campoDepto = "department";
+    String botaoSubmitFormulario = "//*[@id=\"submit\"]";
+
     public void validaExibicaoMenu(String menu) {
         if (menu.equals("forms")) {
-            driver.findElement(By.xpath(Variaveis.menuForms)).isDisplayed();
+            driver.findElement(By.xpath(menuForms)).isDisplayed();
         } else if (menu.equals("alerts, frame & windows")) {
-            driver.findElement(By.xpath(Variaveis.menuAlerts)).isDisplayed();
+            driver.findElement(By.xpath(menuAlerts)).isDisplayed();
         } else if (menu.equals("elements")) {
-            driver.findElement(By.xpath(Variaveis.menuElements)).isDisplayed();
+            driver.findElement(By.xpath(menuElements)).isDisplayed();
         } else if (menu.equals("interactions")) {
-            driver.findElement(By.xpath(Variaveis.menuInteractions)).isDisplayed();
+            driver.findElement(By.xpath(menuInteractions)).isDisplayed();
         } else if (menu.equals("widgets")) {
-            driver.findElement(By.xpath(Variaveis.widgets)).isDisplayed();
+            driver.findElement(By.xpath(widgets)).isDisplayed();
         } else if (menu.equals("book store application")) {
-            driver.findElement(By.xpath(Variaveis.menuBookStoreApplication)).isDisplayed();
+            driver.findElement(By.xpath(menuBookStoreApplication)).isDisplayed();
         }
     }
 
     public void acessaMenu(String menu) {
         if (menu.equals("forms")) {
-            driver.findElement(By.xpath(Variaveis.menuForms)).click();
+            driver.findElement(By.xpath(menuForms)).click();
         } else if (menu.equals("alerts, frame & windows")) {
-            driver.findElement(By.xpath(Variaveis.menuAlerts)).click();
+            driver.findElement(By.xpath(menuAlerts)).click();
         } else if (menu.equals("elements")) {
-            driver.findElement(By.xpath(Variaveis.menuElements)).click();
+            driver.findElement(By.xpath(menuElements)).click();
         } else if (menu.equals("interactions")) {
-            driver.findElement(By.xpath(Variaveis.menuInteractions)).click();
+            driver.findElement(By.xpath(menuInteractions)).click();
         } else if (menu.equals("widgets")) {
-            driver.findElement(By.xpath(Variaveis.widgets)).click();
+            driver.findElement(By.xpath(widgets)).click();
         } else if (menu.equals("book store application")) {
-            driver.findElement(By.xpath(Variaveis.menuBookStoreApplication)).click();
+            driver.findElement(By.xpath(menuBookStoreApplication)).click();
         }
     }
 
     public void validaSubMenu(String menu) {
-        List<String> submenus = Variaveis.getSubmenus(menu);
+        List<String> submenus = getSubmenus(menu);
         for (String submenu : submenus) {
             String submenuXpath = "//span[@class='text' and text()='" + submenu + "']";
             if (driver.findElement(By.xpath(submenuXpath)).isDisplayed()) {
@@ -57,40 +106,40 @@ public class NavegacaoPages extends Utils {
     public void acessaSubMenu(String submenu) {
         if (submenu.equals("web tables")) {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-            WebElement webTablesElement = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(Variaveis.subMenuWebElements)));
+            WebElement webTablesElement = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(subMenuWebElements)));
             webTablesElement.click();
         } else if(submenu.equals("login")){
-            WebElement element = driver.findElement(By.cssSelector(Variaveis.subMenuLogin));
+            WebElement element = driver.findElement(By.cssSelector(subMenuLogin));
             ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
         }
     }
 
     public void acessarOperacao(String operacao) {
         if(operacao.equals("adicionar")){
-            driver.findElement(By.xpath(Variaveis.botaoAddNovoRegistro)).click();
+            driver.findElement(By.xpath(botaoAddNovoRegistro)).click();
         } else if(operacao.equals("editar")){
-            driver.findElement(By.xpath(Variaveis.botaoEditaRegistro)).click();
+            driver.findElement(By.xpath(botaoEditaRegistro)).click();
         } else if(operacao.equals("deletar")){
-            driver.findElement(By.xpath(Variaveis.botaoDeletaRegistro)).click();
+            driver.findElement(By.xpath(botaoDeletaRegistro)).click();
         }
     }
 
     public void realizarOperacao(String acao) throws InterruptedException{
         if(acao.equals("preencho")){
             driver.findElement(By.xpath("//div[4]/div/div")).isDisplayed();
-            driver.findElement(By.id(Variaveis.campoNome)).sendKeys("Bruno");
-            driver.findElement(By.id(Variaveis.campoSobrenome)).sendKeys("Teste");
-            driver.findElement(By.id(Variaveis.campoEmail)).sendKeys("teste@teste.com.br");
-            driver.findElement(By.id(Variaveis.campoIdade)).sendKeys("36");
-            driver.findElement(By.id(Variaveis.campoSalario)).sendKeys("5000");
-            driver.findElement(By.id(Variaveis.campoDepto)).sendKeys("TI");
-            driver.findElement(By.xpath(Variaveis.botaoSubmitFormulario)).click();
+            driver.findElement(By.id(campoNome)).sendKeys("Bruno");
+            driver.findElement(By.id(campoSobrenome)).sendKeys("Teste");
+            driver.findElement(By.id(campoEmail)).sendKeys("teste@teste.com.br");
+            driver.findElement(By.id(campoIdade)).sendKeys("36");
+            driver.findElement(By.id(campoSalario)).sendKeys("5000");
+            driver.findElement(By.id(campoDepto)).sendKeys("TI");
+            driver.findElement(By.xpath(botaoSubmitFormulario)).click();
         } else if(acao.equals("altero")){
             driver.findElement(By.xpath("//div[4]/div/div")).isDisplayed();
-            WebElement campoNome = driver.findElement(By.id(Variaveis.campoNome));
-            campoNome.clear();
-            campoNome.sendKeys("Alteracao");
-            driver.findElement(By.xpath(Variaveis.botaoSubmitFormulario)).click();
+            WebElement campoNomeAlteracao = driver.findElement(By.id(campoNome));
+            campoNomeAlteracao.clear();
+            campoNomeAlteracao.sendKeys("Alteracao");
+            driver.findElement(By.xpath(botaoSubmitFormulario)).click();
         } else if(acao.equals("excluo")){
             while (true) {
                 List<WebElement> deleteButtons = driver.findElements(By.xpath("//*[contains(@id, 'delete-record-')]"));
@@ -120,11 +169,11 @@ public class NavegacaoPages extends Utils {
     }
 
     public void pesquisaRegistro(String registro){
-        driver.findElement(By.xpath(Variaveis.campoPesquisa)).sendKeys(registro);
+        driver.findElement(By.xpath(campoPesquisa)).sendKeys(registro);
     }
 
     public void validaPesquisa(String registro){
-        for (String[] valores : Variaveis.REGISTROS) {
+        for (String[] valores : REGISTROS) {
             if (registro.equals(valores[0])) {
                 validarElementos(valores);
                 break;
